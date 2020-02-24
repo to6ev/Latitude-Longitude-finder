4706d8c0703d2f5f02c3cacc37a2786ed36672bb
Evgeni Toshev <test_chitalishte@abv.bg>
2020-02-25 00:10:50 +0200

    Update latitude-longitude-finder.php

--- latitude-longitude-finder.php
+++ latitude-longitude-finder.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Simple Latitude Longitude finder</title>
+<title>Latitude Longitude finder</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="Map Latitude Longitude finder. Online tool to find the latitude and longitude of a location chosen on a map">
 <meta name="keywords" content="latitude longitude finder, latitude longitude lookup, show latitude longitude on map, find latitude and longitude"/>
 <meta name="author" content="www.eti.pw">
-<meta name="robots" content="all"/>
 </head>
 
 <body>
@@ -30,11 +29,8 @@ print ('<b>Map Latitude Longitude:</b>
 <small>e.g. 27.3717 -81.4306</small>
 </form>');
 
-// We will use free google "api" :) Keyless access to Google Maps Platform is deprecated since June 11th, 2018
-// Get Key if You want from: https://cloud.google.com/maps-platform/
-
-// NO NEED KEY WITH THIS SIMPLE FORM
-echo "<iframe src='https://google-developers.appspot.com/maps/documentation/utils/geocoder/#q%3D{$latitude}%2520{$longitude}' width='100%' height='900' FRAMEBORDER=NO FRAMESPACING=0 BORDER=0 ></iframe>";
+// echo "<iframe src='https://google-developers.appspot.com/maps/documentation/utils/geocoder/#q%3D{$latitude}%2520{$longitude}' width='100%' height='900' FRAMEBORDER=NO FRAMESPACING=0 BORDER=0 ></iframe>";
+echo "<iframe src='https://developers-dot-devsite-v2-prod.appspot.com/maps/documentation/utils/geocoder?hl=pt-br#q%3D{$latitude}%2520{$longitude}' width='100%' height='900' FRAMEBORDER=NO FRAMESPACING=0 BORDER=0 ></iframe>";
 
 }
 
